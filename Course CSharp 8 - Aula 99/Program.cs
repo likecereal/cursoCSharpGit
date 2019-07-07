c4d8674e1c2e1cb589dfdbc98a1f8effda88fd55
Aires Menezes <aires.ribeiro.menezes@hotmail.com>
2019-07-07 14:30:33 +0100

    Projecto iniciado, para mostrar ola mundo

---
+++ Course CSharp 8 - Aula 99/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Course_CSharp_8___Aula_99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
